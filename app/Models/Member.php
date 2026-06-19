c0ae1309bd5a2e4d8b81c9f1dbc88ef1f6bf2161
renasyach <117002888+renasyach@users.noreply.github.com>
2026-06-19 23:42:05 +0700

    fix:update

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Member extends Model
 {
+    // DB lama tidak punya kolom updated_at
+    const UPDATED_AT = null;
+
     protected $fillable = [
         'nama',
         'nim',
